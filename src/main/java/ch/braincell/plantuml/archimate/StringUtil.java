ee260fc64aeb2d0055ca59135fc3e63d70d575f4
boessu <boessu@users.noreply.github.com>
2023-10-25 18:17:53 +0200

    Optimizing wrapper
    Adding JUnit test.
    Wrapper doesn't wrap creole links now.

--- src/main/java/ch/braincell/plantuml/archimate/StringUtil.java
+++ src/main/java/ch/braincell/plantuml/archimate/StringUtil.java
@@ -5,13 +5,18 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 public class StringUtil {
 
-	private final static String SKIPPING_LISTS = "^\\s*[*#=]+[\\s].*"; // for skipping lists (can't be wrapped) Lists
-																		// need at least a space to be before text to be
-																		// recognized.
+	private final static Pattern SKIPPING_LISTS = Pattern.compile("^\\s*[*#=]+[\\s].*"); // for skipping lists (can't be
+																							// wrapped) Lists
+	// need at least a space to be before text to be
+	// recognized.
 	private final static String[] TOKEN = { "**", "//", "\"\"", "--", "__", "~~" };
+
+	private final static String LINK_OPEN = "[[";
+	private final static String LINK_CLOSE = "]]";
 
 	/**
 	 * Wraps a String with specific length and some sort of creole sensitivity from
@@ -28,7 +33,8 @@ public class StringUtil {
 	/**
 	 * Wraps a String with specific length and some sort of creole sensitivity from
 	 * plantuml. This wrapper can't fix curious scenarios with overlapping creole
-	 * syntax (plantuml can't handle that too). TODO: Should be better optimized.
+	 * syntax (plantuml can't handle that too). TODO: Looks rather ugly (but works
+	 * as expected). Should be better optimized.
 	 * 
 	 * @param input      input string to word wrap
 	 * @param wrapLength length to wrap the lines. the longest word will overwrite
@@ -38,17 +44,22 @@ public class StringUtil {
 	 * @return text wrapped with newLine.
 	 */
 	public static String wrap(String input, int wrapLength, String newLine) {
+		if (input == null || input.isEmpty())
+			return null;
+
 		StringBuffer result = new StringBuffer(input.length() + 32);
 
 		wrapLength = getMaxWordLength(input, wrapLength, 50);
 
-		String[] splitter = input.split("[\\r\\n]{1,2}");
+		String[] splitter = input.split("\\r\\n|\\n\\r|\\r|\\n");
 
 		boolean nextLine = false;
 		for (String line : splitter) {
-			if (nextLine)
+			if (nextLine || line.isEmpty()) {
+				trimEnd(result);
 				result.append(newLine);
-			if (line.length() <= wrapLength || line.matches(SKIPPING_LISTS)) {
+			}
+			if (line.length() <= wrapLength || SKIPPING_LISTS.matcher(line).matches()) {
 				result.append(line);
 			} else {
 				int lineLength = 0;
@@ -57,50 +68,79 @@ public class StringUtil {
 				List<String> formats = new ArrayList<>(TOKEN.length);
 				List<String> toRemove = new ArrayList<>(TOKEN.length);
 				List<String> toAdd = new ArrayList<>(TOKEN.length);
+				boolean openLink = false;
 				while (toktok.hasMoreTokens()) {
 					String token = toktok.nextToken();
-					for (String search : TOKEN) {
-						int creoleCount = countFindings(token, search);
-						if (creoleCount > 0) {
-							creoleLength += creoleCount * search.length();
-							if (creoleCount % 2 != 0) {
-								if (formats.contains(search)) {
-									toRemove.add(search);
-								} else {
-									toAdd.add(search);
+					if (token.startsWith(LINK_OPEN)) {
+						openLink = true;
+					}
+
+					if (openLink) {
+						result.append(token);
+						lineLength += token.length();
+						if (token.endsWith(LINK_CLOSE))
+							openLink = false;
+						else {
+							result.append(' ');
+							lineLength++;
+						}
+					} else {
+						// testing creole token
+						for (String creoleToken : TOKEN) {
+							int creoleCount = countFindings(token, creoleToken);
+							if (creoleCount > 0) {
+								creoleLength += creoleCount * creoleToken.length();
+								if (creoleCount % 2 != 0) {
+									// creole token is open
+									if (formats.contains(creoleToken)) {
+										toRemove.add(creoleToken);
+									} else {
+										toAdd.add(creoleToken);
+									}
 								}
 							}
 						}
-					}
-					if ((lineLength + token.length() - creoleLength) < wrapLength) {
-						result.append(token).append(" ");
-						lineLength += token.length() + 1;
-						formats.addAll(toAdd);
-						toAdd.clear();
-						formats.removeAll(toRemove);
-						toRemove.clear();
-					} else if (!result.isEmpty()) { //only finish if the last line is not empty
-						result.setLength(result.length() - 1);
-						for (int i = formats.size(); i > 0; i--)
-							result.append(formats.get(i - 1));
-						result.append(newLine);
-						formats.forEach(f -> result.append(f));
-						formats.addAll(toAdd);
-						toAdd.clear();
-						formats.removeAll(toRemove);
-						toRemove.clear();
-						result.append(token).append(" ");
-						creoleLength = 0;
-						for (String format : formats)
-							creoleLength += format.length();
-						lineLength = token.length() + creoleLength;
+
+						if ((lineLength + token.length() - creoleLength) < wrapLength) {
+							result.append(token).append(' ');
+							lineLength += token.length() + 1;
+							formats.addAll(toAdd);
+							toAdd.clear();
+							formats.removeAll(toRemove);
+							toRemove.clear();
+						} else if (!result.isEmpty()) { // only finish if the line is not empty
+							trimEnd(result);
+							for (int i = formats.size(); i > 0; i--)
+								result.append(formats.get(i - 1));
+							result.append(newLine);
+							formats.forEach(f -> result.append(f));
+							formats.addAll(toAdd);
+							toAdd.clear();
+							formats.removeAll(toRemove);
+							toRemove.clear();
+							result.append(token).append(" ");
+							creoleLength = 0;
+							for (String format : formats)
+								creoleLength += format.length();
+							lineLength = token.length() + creoleLength + 1;
+						}
 					}
 				}
 			}
 			nextLine = true;
 		}
-
+		trimEnd(result);
 		return result.toString();
+	}
+
+	/**
+	 * trims spaces of a StringBuffer at the end.
+	 * 
+	 * @param bufferToTrim
+	 */
+	private static void trimEnd(StringBuffer bufferToTrim) {
+		while (bufferToTrim.charAt(bufferToTrim.length() - 1) == ' ')
+			bufferToTrim.setLength(bufferToTrim.length() - 1);
 	}
 
 	/**
---
+++ src/test/java/ch/braincell/plantuml/archimate/StringUtilTest.java
@@ -0,0 +1,31 @@
+package ch.braincell.plantuml.archimate;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+class StringUtilTest {
+	@Test
+	void test() {
+		String test = "**bold with more that 10 chars**\n* this is a bullet list and this line won't be wrapped.\n**If it is Bold from the beginning, it will be wrapped.**\n\n[[https:www.heise.de/newsticker this is a newsticker link and won't be wrapped.]] This is a **//~~string with some strange behaviour and~~//** some very long, long, long text and a //~~new line~~ in it//.";
+
+		String[] expectedResult = {
+				"**bold with more that 10 chars**",
+				"* this is a bullet list and this line won't be wrapped.",
+				"**If it is Bold from the**",
+				"**beginning, it will be wrapped.**",
+				"",
+				"[[https:www.heise.de/newsticker this is a newsticker link and won't be wrapped.]]",
+				"This is a **//~~string with some~~//**",
+				"**//~~strange behaviour and~~//** some",
+				"very long, long, long text and",
+				"a //~~new line~~ in it//."
+		};
+		
+		String result = StringUtil.wrap(test, 10, "\n");
+
+		String[] lines = result.split("\n");
+		assertArrayEquals(lines, expectedResult);
+	}
+
+}
